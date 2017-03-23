c104084f45b389bad3b60bfc09b3c46ee806357a
Patrick Ray <pamiray@umich.edu>
2017-03-23 11:59:05 -0400

    messing with coveralls

--- rpn.py
+++ rpn.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import operator
+import readline
+import random
+from termcolor import colored, cprint
+
 
 OPERATORS = {
 	'+': operator.add,
@@ -31,7 +35,20 @@ def calculate(arg):
 def main():
 	while True:
 		result = calculate(input('rpn calc> '))
-		print("Result:", result)
+		str_result = str(result)
+		arr = []
+		arr.append('grey')
+		arr.append('red')
+		arr.append('green')
+		arr.append('yellow')
+		arr.append('blue')
+		arr.append('magenta')
+		arr.append('cyan')
+		arr.append('white')
+		val = random.randint(0, 7)
+
+		str_result = colored(str_result, arr[val])
+		print("Result:", str_result)
 
 if __name__ == '__main__':
 	main()
